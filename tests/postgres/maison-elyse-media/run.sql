670504177eddf69fa2351d6cdd04091c1652be24
Marc Saad-Hadidi <246904446+MarcSaad-Hadidi@users.noreply.github.com>
2026-07-21 22:38:49 -0400

    test: grant postgres harness helpers to service role

--- tests/postgres/maison-elyse-media/run.sql
+++ tests/postgres/maison-elyse-media/run.sql
@@ -1,6 +1,8 @@
 \set ON_ERROR_STOP on
 \ir ../../../supabase/migrations/0013_create_owner_restaurant_with_menu.sql
 \ir ../../../supabase/migrations/20260721150000_maison_elyse_media_backfill_rpc.sql
+grant usage on schema qr_test to service_role;
+grant execute on all functions in schema qr_test to service_role;
 \ir fixture.sql
 \ir security.test.sql
 \ir behavior.test.sql
